9792bbac7154535fbfad4c5e0eb30b97d7b46085
mayoumelon <mayoumelon@gmail.com>
2022-01-20 08:40:51 +0900

    add bcrypt

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,13 @@
 const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
 
 const registerUser = async (req, res) => {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   try {
