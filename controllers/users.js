31320971c8c9cefa47b8f92790841acf75b39856
mayoumelon <mayoumelon@gmail.com>
2022-01-20 17:36:49 +0900

    create Token

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 const User = require("../models/User.js");
 const bcrypt = require("bcrypt");
+const { JsonWebTokenError } = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const registerUser = async (req, res) => {
   const saltRounds = 10;
@@ -18,16 +20,25 @@ const registerUser = async (req, res) => {
   }
 };
 
+const generateAccessToken = (user) => {
+  const payload = {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+  };
+  return jwt.sign(payload, process.env.TOKEN_SECRET, {
+    expiresIn: '1d'
+  });
+};
+
 const loginUser = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(401).json("メールアドレスに誤りがあります");
   const match = await bcrypt.compare(req.body.password, user.password);
   if (!match) return res.status(401).json("パスワードに誤りがあります。");
   res.status(200).json({
-    id: user._id,
-    name: user.name,
-    email: user.email,
+    token: generateAccessToken(user),
   });
-}
+};
 
 module.exports = { registerUser, loginUser };
